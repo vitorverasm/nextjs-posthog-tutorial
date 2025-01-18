aa6b347ba35702224a44b1ee595b11f7af2adb87
Vitor Veras <vitorverasm@gmail.com>
2025-01-18 00:32:47 -0300

    feat: add posthog

---
+++ app/providers.tsx
@@ -0,0 +1,23 @@
+// app/providers.jsx
+"use client";
+
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+import { useEffect } from "react";
+
+export default function PostHogProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        capture_pageview: false,
+      });
+    }
+  }, []);
+
+  return <PHProvider client={posthog}>{children}</PHProvider>;
+}
